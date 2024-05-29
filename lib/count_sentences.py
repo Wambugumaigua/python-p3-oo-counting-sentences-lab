5548ad79fac0b5f105ea3b7c4c96f93d0904e6ac
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-05-29 05:26:39 -0700

    complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise TypeError("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        
+        temp_value = self.value.replace('?', '.').replace('!', '.')
+       
+        sentences = temp_value.split('.')
+        
+        valid_sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(valid_sentences)
